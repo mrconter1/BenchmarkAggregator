4c957403089b12549b4b1d311db480a83223479c
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-08-19 16:47:01 +0200

    Add more models to main

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ async def main():
 
     # Specify which models to evaluate 
     models = [
+        "openai/gpt-3.5-turbo-0613"
         "openai/gpt-4o-mini-2024-07-18",
         "openai/gpt-4o-2024-08-06"
+        "anthropic/claude-3-sonnet",
+        "anthropic/claude-3-opus",
         "anthropic/claude-3.5-sonnet",
+        "google/gemini-pro-1.5-exp",
+        "meta-llama/llama-3.1-70b-instruct",
+        "meta-llama/llama-3.1-405b-instruct",
+        "mistralai/mistral-large",
     ]
 
     # Specify which benchmarks to run
