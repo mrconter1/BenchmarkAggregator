02c723046cb3c8f6a09ee8dd9325f3c62e6a77e8
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-08-20 08:30:19 +0200

    Clean up main

--- main.py
+++ main.py
@@ -6,24 +6,22 @@ async def main():
 
     # Specify which models to evaluate 
     models = [
-        "openai/gpt-3.5-turbo-0613"
+        #"openai/gpt-3.5-turbo-0613"
         "openai/gpt-4o-mini-2024-07-18",
-        "openai/gpt-4o-2024-08-06"
-        "anthropic/claude-3-sonnet",
-        "anthropic/claude-3-opus",
-        "anthropic/claude-3.5-sonnet",
-        "google/gemini-pro-1.5-exp",
-        "meta-llama/llama-3.1-70b-instruct",
-        "meta-llama/llama-3.1-405b-instruct",
-        "mistralai/mistral-large",
+        #"openai/gpt-4o-2024-08-06"
+        #"anthropic/claude-3-sonnet",
+        #"anthropic/claude-3-opus",
+        #"anthropic/claude-3.5-sonnet",
+        #"google/gemini-pro-1.5-exp",
+        #"meta-llama/llama-3.1-70b-instruct",
+        #"meta-llama/llama-3.1-405b-instruct",
+        #"mistralai/mistral-large",
     ]
 
     # Specify which benchmarks to run
     benchmark_ids = [
         "MMLU-Pro",
         "GSM8K",
-        "GPQA-Diamond",
-        "DROP"
     ]
 
     # Run the benchmarks
